0a569000fc9b524dcee82a3918a208082d8e772f
Tanishq Patil <trpatil@ucsc.edu>
2025-04-07 17:46:06 -0700

    fixing yolo trainer

--- VTUAV-Scripts/YOLO_train.py
+++ VTUAV-Scripts/YOLO_train.py
@@ -1,5 +1,5 @@
 import sys
 from ultralytics4channel import YOLO
 if __name__ == "__main__":
-    model = YOLO("./yolo8s.yaml")
+    model = YOLO("yolov8s.pt")
     results = model.train(data="data.yaml", epochs=100, imgsz=640, device=[0, 1])
