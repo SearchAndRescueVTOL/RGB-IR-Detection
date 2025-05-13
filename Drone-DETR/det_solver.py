de16184288209734c4e9938b64349d2872bf0749
Tanishq Patil <trpatil@ucsc.edu>
2025-05-13 16:22:08 -0700

    bug fix

--- Drone-DETR/det_solver.py
+++ Drone-DETR/det_solver.py
@@ -206,8 +206,8 @@ if __name__ == "__main__":
 
     train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True, collate_fn=collate_fn)
     val_loader = DataLoader(val_dataset, batch_size=8, shuffle=False, collate_fn=collate_fn)
-    optim = torch.optim.AdamW(model.parameters(), lr=1e4, weight_decay=1e4).to(device)
-    lr_sched = torch.optim.lr_scheduler.CosineAnnealingLR(optim, T_max=400, eta_min = 0).to(device)
+    optim = torch.optim.AdamW(model.parameters(), lr=1e4, weight_decay=1e4)
+    lr_sched = torch.optim.lr_scheduler.CosineAnnealingLR(optim, T_max=400, eta_min = 0)
     cfg = SimpleNamespace(checkpoint_freq=10,
                             train_dataloader=train_loader,
                             val_dataloader=val_loader,
