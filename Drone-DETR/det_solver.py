4cef554a4cc3888500a9a0c5ba42d06e599a5ccc
Tanishq Patil <trpatil@ucsc.edu>
2025-05-14 01:35:44 -0700

    test

--- Drone-DETR/det_solver.py
+++ Drone-DETR/det_solver.py
@@ -243,7 +243,7 @@ if __name__ == "__main__":
     # val_sampler = DistributedSampler(val_dataset, shuffle=False)
     train_loader = DataLoader(train_dataset, shuffle=True, batch_size=8, collate_fn=collate_fn)
     val_loader = DataLoader(val_dataset, shuffle=False, batch_size = 8, collate_fn=collate_fn)
-    optim = torch.optim.AdamW(model.parameters(), lr=1e4, weight_decay=1e4)
+    optim = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=1e-4)
     lr_sched = torch.optim.lr_scheduler.CosineAnnealingLR(optim, T_max=400, eta_min = 0)
     cfg = SimpleNamespace(checkpoint_freq=10,
                             train_dataloader=train_loader,
