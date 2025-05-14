ed0d29ae7bb894afb221680b89e04f943909a7f9
Tanishq Patil <trpatil@ucsc.edu>
2025-05-14 00:18:23 -0700

    test

--- Drone-DETR/criterion.py
+++ Drone-DETR/criterion.py
@@ -255,11 +255,11 @@ class SetCriterion(nn.Module):
 
         # Compute all the requested losses
         losses = {}
+        print(self.losses)
         for loss in self.losses:
             l_dict = self.get_loss(loss, outputs, targets, indices, num_boxes)
             l_dict = {k: l_dict[k] * self.weight_dict[k] for k in l_dict if k in self.weight_dict}
             losses.update(l_dict)
-        print(losses)
         # In case of auxiliary losses, we repeat this process with the output of each intermediate layer.
         if 'aux_outputs' in outputs:
             for i, aux_outputs in enumerate(outputs['aux_outputs']):
